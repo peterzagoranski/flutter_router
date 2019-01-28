7465c8c0ae4f2def8f9242f17bb92cdce533f78b
Peter Zagoranski <peter.zagoranski@gmail.com>
2019-01-28 09:33:20 +0100

    v1.0.5

--- lib/flutter_router.dart
+++ lib/flutter_router.dart
@@ -6,25 +6,26 @@ import 'package:uri/uri.dart';
 
 typedef Widget RouterBuilder(BuildContext context, UriMatch match);
 
-class Router extends _Router {
-  Router(final Map<String, RouterBuilder> definitions) : super(definitions, page: (settings, builder) => MaterialPageRoute<dynamic>(settings: settings, builder: builder));
+/*class Router extends _Router {
+  Router(final Map<String, RouterBuilder> definitions) : super(definitions, page: (settings, builder) => MaterialPageRoute<dynamic>(builder: builder, settings: settings));
 }
 
 class CupertinoRouter extends _Router {
-  CupertinoRouter(final Map<String, RouterBuilder> definitions) : super(definitions, page: (settings, builder) => CupertinoPageRoute<dynamic>(settings: settings, builder: builder));
-}
+  CupertinoRouter(final Map<String, RouterBuilder> definitions) : super(definitions, page: (settings, builder) => new CupertinoPageRoute(settings: settings, builder: builder));
+}*/
 
-abstract class _Router {
+class Router {
 
-  _Router(final Map<String, RouterBuilder> definitions, { this.page }) : assert(null != definitions || null != page), this.definitions = definitions;
+  Router(final Map<String, RouterBuilder> definitions) : assert(null != definitions), this.definitions = definitions;
 
   Route<dynamic> get(final RouteSettings settings) {
     final matches = this.definitions.keys.where((route) => UriParser(UriTemplate(route)).matches(Uri.parse(settings.name)));
     final route = null != matches && matches.length > 0 ? matches.first : null;
 
-    return null != route ? this.page(settings, (context)=> this.definitions[route](context, UriParser(UriTemplate(route)).match(Uri.parse(settings.name)))) : null;
+    final PageRouteFactory page = (settings, builder) => MaterialPageRoute(builder: builder, settings: settings);
+
+    return null != route ? page(settings, (context)=> this.definitions[route](context, UriParser(UriTemplate(route)).match(Uri.parse(settings.name)))) : null;
   }
 
   final Map<String, RouterBuilder> definitions;
-  final PageRouteFactory page;
 }
